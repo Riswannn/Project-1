f7525a2483f6a87d460100c1bbe5a108d5dad117
Your Name <you@example.com>
2023-03-10 08:17:58 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart' show BuildContext, MaterialApp, State, StatefulWidget, Widget, runApp;
+import 'package:flutter_application_2/home.dart';
+import 'package:flutter_application_2/kutipan.dart';
+import 'package:flutter_application_2/login.dart';
+import 'package:flutter_application_2/matkul.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/home":(context) => const Home(),
+        "/matkul":(context) => const Matkul(),
+        "/":(context) => const Login(),
+        "/kutipan":(context) => const Kutipan(),
+      },
+
+    );
+  }
+}
